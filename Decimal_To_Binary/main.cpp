83d9a371d801e99a0c621f95f127d7cf892f8136
Nicholas Monteiro Vital <53572708+nichmontvit@users.noreply.github.com>
2020-08-10 17:23:58 +0200

    Add files via upload

---
+++ Decimal_To_Binary/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int number = 0;
+    cout << "Enter a number: ";
+    cin >> number;
+    int remainder, binary = 0, var = 1;
+
+    do{
+        remainder = number % 2;
+        number = number / 2 ;
+        binary = binary + (remainder * var);
+        var *= 10;
+    }while(number > 0);
+
+    cout << "the binary number is: " << binary << "\n";
+    return 0;
+}
